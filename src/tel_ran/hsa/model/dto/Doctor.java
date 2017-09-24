6565c2366b239e56cfe6ee0f4262eb392543775f
Gantverg <gantvergroman@gmail.com>
2017-09-25 01:35:19 +0300

    add Set<DayOfWeek>, getter, setter & method isDayWorking to doctor Entity: for building schedule

--- src/tel_ran/hsa/model/dto/Doctor.java
+++ src/tel_ran/hsa/model/dto/Doctor.java
@@ -1,9 +1,24 @@
 package tel_ran.hsa.model.dto;
 
+import java.time.*;
+import java.util.*;
+
 public class Doctor extends Person {
+	Set<DayOfWeek> workingDays;
 
 	public Doctor(int id, String name, String phoneNumber, String eMail) {
 		super(id, name, phoneNumber, eMail);
 	}
+	
+	public void setWorkingDays(DayOfWeek... days) {
+		workingDays = new HashSet<>(Arrays.asList(days));
+	}
 
+	public Iterable<DayOfWeek> getWorkingDays() {
+		return workingDays;
+	}
+	
+	public boolean isDayWorking(LocalDate date) {
+		return workingDays.contains(date.getDayOfWeek());
+	}
 }
